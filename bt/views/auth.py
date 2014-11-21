df66dffcf84147a0c17320d3064263cd448956ef
Daniel M. Segundo <daniel.mtzx@gmail.com>
2014-11-20 19:22:18 -0600

    Agrega next y redirect
    Se agrega next y redirect a template de login

--- bt/views/auth.py
+++ bt/views/auth.py
@@ -18,6 +18,7 @@ def login_user(request):
             if user.is_active:
                 login(request, user)
                 state = "You're successfully logged in!"
+                return HttpResponseRedirect('/')
             else:
                 state = "Your account is not active, please contact the site admin."
         else:
