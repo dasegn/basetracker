02a627499458a08baa542d9d348d19d77575dcbf
Daniel M. Segundo <daniel.mtzx@gmail.com>
2014-11-26 16:49:28 -0600

    Templates de proyecto y perfil
    Se agregan las plantillas de proyecto y perfil en frontend

--- bt/views/profiles.py
+++ bt/views/profiles.py
@@ -2,19 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
 from django.contrib.auth.decorators import login_required
+from utils.helpers import CurrentUsr
 
 # Create your views here.
-@login_required
-def index(request):
-	template = loader.get_template('admin/profiles/index.html')
-	context = Context({
-		'variable': 'desde vista',
-	})
-	return HttpResponse(template.render(context))
+#@login_required
+#def index(request):
+#	template = loader.get_template('admin/profiles/index.html')
+#	context = Context({
+#		'variable': 'desde vista',
+#	})
+#	return HttpResponse(template.render(context))
 
 def detail(request, profile_id):
-	template = loader.get_template('admin/profiles/index.html')
+	template = loader.get_template('profiles.html')
 	context = Context({
+		'cuser': CurrentUsr(request.user),
 		'profile_id': profile_id,
 	})	
 	return HttpResponse(template.render(context))
