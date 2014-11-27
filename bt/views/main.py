8b560328421863e83091d16e4ee3a47485187352
Ivar Hernández <ivar.hdez@gmail.com>
2014-11-27 17:53:44 -0600

    Final Dash
    Va con la versión final incluyendo tabla de acciones

--- bt/views/main.py
+++ bt/views/main.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
+from utils.logentries import ListLogEntries
 from utils.helpers import CurrentUsr
 
 def index(request):
 	template = loader.get_template('main.html')
+	log_entries = ListLogEntries(9)	
 	context = Context({
 		'cuser': CurrentUsr(request.user),
+		'entries': log_entries,		
 	})
 	return HttpResponse(template.render(context))
 
