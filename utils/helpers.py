cadab373eb35bcd3a1ad27bb5bf7eded28ad137c
Daniel M. Segundo <daniel.mtzx@gmail.com>
2014-12-02 12:28:00 -0600

    Tags para semanas en selector de fechas
    Se agregan tags para semanas y construcción de URLs para seleccionar
    fechas en el dashboard

--- utils/helpers.py
+++ utils/helpers.py
@@ -24,25 +24,30 @@ class GetActiveWeek(object):
 	def __init__(self,request):
 		year = request.GET.get('year', None)
 		week = request.GET.get('week', None)		
-		date_pattern = '%d-%m-%Y'
+		self.date_pattern = '%d/%m/%y'
 
 		if year is None and week is None:
 			self.week_now = datetime.now()
 		else:
 			self.week_now = self.get_first_day(int(year), int(week))
 
-		self.week_last = (self.week_now - timedelta(days=7)).strftime(date_pattern)	
-		self.week_next = (self.week_now + timedelta(days=7)).strftime(date_pattern)
-		self.week_now = self.week_now.strftime(date_pattern)
+		self.week_last_two = self.get_week_range(self.week_now - timedelta(days=14))
+		self.week_last = self.get_week_range(self.week_now - timedelta(days=7))
+		self.week_next = self.get_week_range(self.week_now + timedelta(days=7))
+		self.week_next_two = self.get_week_range(self.week_now + timedelta(days=14))
+		self.week_now = self.get_week_range(self.week_now)
 
 	def get_week_range(self, week_date):
 		year, week, dow = week_date.isocalendar()
 		if dow == 1:
 			start_date = week_date
 		else:
-			start_date = week_date - timedelta(dow)
+			start_date = self.get_first_day(year,week)
 		end_date = start_date + timedelta(6)
-		return (start_date, end_date)
+
+		start_date = start_date.strftime(self.date_pattern)	
+		end_date = end_date.strftime(self.date_pattern)	
+		return (start_date, end_date, year, week)
 
 	def get_first_day(self, year, weeknum):
 		ret = datetime.strptime('%04d-%02d-1' % (year, weeknum), '%Y-%W-%w')
