2d779462a02e39ca5fb4151525c157993768eba7
dnlms <daniel@MacBook-Air-de-Daniel.local>
2014-12-08 23:27:41 -0600

    Correcciones varias a plugin weekpicker
    Se corrige la selección de fecha que impedía seleccionar el día lunes
    de cada semana y devolvía la semana incorrecta, se agrega un método
    para calcular fielmente el año de la lista partiendo de fechas
    seleccionadas

--- bt/static/js/bt.tasklist.js
+++ bt/static/js/bt.tasklist.js
@@ -1,27 +1,51 @@
 // Chosen init file
 (function($){
 	jQuery(document).ready(function(){
-		var month_names = django.catalog["January February March April May June July August September October November December"];
-		var day_names = django.catalog["S M T W T F S"];
-		
+		jQuery.datepicker.regional['es'] = {
+			 closeText: 'Cerrar',
+			 prevText: '<Ant',
+			 nextText: 'Sig>',
+			 currentText: 'Hoy',
+			 monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+			 monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+			 dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+			 dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+			 dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+			 weekHeader: 'Sm',
+			 dateFormat: 'dd/mm/yy',
+			 firstDay: 1,
+			 isRTL: false,
+			 showMonthAfterYear: false,
+			 yearSuffix: ''
+		 };		
 		jQuery.datepicker.setDefaults( jQuery.datepicker.regional[ "es" ] );
+
 		jQuery('.weekpicker').weekpicker({
 			firstDay: 1,
 			weekLength: 7,
-			//calculateWeek: myWeekCalc,
+			showWeek: true,
 			startField: jQuery('.startField'),
 			endField: jQuery('.endField'),
 			dateFormat: 'yy-mm-dd',
-			monthNames: month_names.split(" "),
-			dayNamesMin: day_names.split(" "),
 	        onSelect: function(dateText, inst) {
-	            var weekNumber = jQuery.datepicker.iso8601Week(new Date(dateText));
-	            console.log(weekNumber);
-	            console.log(jQuery.datepicker.formatDate('D', new Date(dateText)));
-	            var year = jQuery(this).datepicker('getDate').getFullYear();
-	            jQuery('.listName').val("Semana " + weekNumber + ' ' + year);
-	            
+	        	seldate = $.datepicker.parseDate( "yy-mm-dd", dateText);	        	
+	            var weekNumber = jQuery.datepicker.iso8601Week(seldate);	            
+	            var year = getRealYear(jQuery(this).datepicker('getDate'));	            
+	            jQuery('.listName').val("Semana " + weekNumber + ' ' + year);	            
 	        }			
 		});
+
+		function getRealYear(dates) {
+			var checkDate = new Date(dates.getTime());
+			// Find Thursday of this week starting on Sunday
+			checkDate.setDate(checkDate.getDate() + 4 - (checkDate.getDay()));
+			var time = checkDate.getTime();
+			checkDate.setMonth(0); // Compare with Jan 1
+			checkDate.setDate(1);
+			//return (Math.floor(Math.round((time - checkDate) / 86400000) / 7) + 1);
+			return checkDate.getFullYear();
+		}
+
+
 	});
 })((typeof window.django != 'undefined') ? django.jQuery : jQuery);
