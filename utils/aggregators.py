f8e12e4232e2f5b60f466a274fda38d172ef2e08
dnlms <daniel@MacBook-Air-de-Daniel.local>
2014-12-23 01:43:15 -0600

    Dummy Aggregate expressions
    Prueba dummy de aggregate expressions, falta revisar la implementación
    en manager para reemplazar llamadas RAW

--- utils/aggregators.py
+++ utils/aggregators.py
@@ -18,3 +18,13 @@ class SQLConcat(SQLAggregate):
             return '%(function)s(%(field)s, "%(separator)s")'
         else:
             return '%(function)s(%(field)s)'
+
+class CompleteCountAggregate(SQLAggregate):
+    is_ordinal = True
+    sql_function = 'WEEK' # unused
+    sql_template = "COUNT(CASE WHEN `bt_task`.`completed` = '1'  THEN `bt_task`.`id` ELSE null END)"
+
+class CompleteCount(Aggregate):
+    name = 'Complete'
+    def add_to_query(self, query, alias, col, source, is_summary):
+        query.aggregates[alias] = CompleteCountAggregate(col, source=source, is_summary=is_summary, **self.extra)
